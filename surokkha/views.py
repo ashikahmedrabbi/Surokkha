08d4d21db6dc2264d2865d6a5a7452cb48c90572
unknown <ashikahmedrabbi@gmail.com>
2024-03-10 01:48:50 +0600

    Update

--- surokkha/views.py
+++ surokkha/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from doctor.models import Doctor
 
 
 def home(request):
-    return render(request, 'home.html')
+    doctors = Doctor.objects.all() 
+    return render(request, 'home.html', {'doctors': doctors})
     
