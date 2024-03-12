af9cb7d0e10ddbce8033c509221f9b2561c8b7c2
unknown <ashikahmedrabbi@gmail.com>
2024-03-12 10:55:04 +0600

    link updated

--- surokkha/settings.py
+++ surokkha/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-['https://surokkha.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS =['https://surokkha.onrender.com','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
